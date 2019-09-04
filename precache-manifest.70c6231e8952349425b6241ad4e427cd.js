adcb601df383137c492180f4f41cdd359ef51369
gemy91 <ag.amrgamal91@gmail.com>
2019-09-04 16:28:34 +0300

    Updates

---
+++ precache-manifest.70c6231e8952349425b6241ad4e427cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76b61a57f5ccdb307b6fd2bd803c236",
+    "url": "/tinder/index.html"
+  },
+  {
+    "revision": "ea2c6b777bff2530c55c",
+    "url": "/tinder/static/css/main.d6d7825e.chunk.css"
+  },
+  {
+    "revision": "42584584f06d829450ac",
+    "url": "/tinder/static/js/2.75f46940.chunk.js"
+  },
+  {
+    "revision": "ea2c6b777bff2530c55c",
+    "url": "/tinder/static/js/main.4a3863f7.chunk.js"
+  },
+  {
+    "revision": "37532ff0914f786d99d7",
+    "url": "/tinder/static/js/runtime~main.123d3f50.js"
+  }
+]);
